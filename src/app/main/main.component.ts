ba46a5723f88b82339c611812e4b0b6b5026d331
nico-4164 <nicolaschaves1991@gmail.com>
2023-07-11 21:10:41 -0300

    formulario

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
+
 })
 export class MainComponent {
 
